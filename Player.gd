22e92f12a1a625e36b2be505d03a21557299ee03
Slowb33 <43762999+slowb33@users.noreply.github.com>
2020-06-12 23:41:18 +0200

    Added AnimationTree States

--- Player.gd
+++ Player.gd
@@ -6,7 +6,10 @@ export var FRICTION = 400
 
 var player
 
-onready var anim = $AnimationPlayer
+onready var animPlayer = $AnimationPlayer
+onready var animTree = $AnimationTree
+onready var animState = animTree.get("parameters/playback")
+
 var velocity = Vector2.ZERO  # The player's movement vector.
 
 # warning-ignore:unused_argument
@@ -21,12 +24,23 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animTree.set("parameters/Idle/blend_position", input_vector)
+		animTree.set("parameters/Walk/blend_position", input_vector)
+		animTree.set("parameters/Jump/blend_position", input_vector)
+		
+		if Input.is_action_pressed(player+"_ui_jump"):
+			animState.travel("Jump")
+		else:
+			animState.travel("Walk")
+			
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 
+# warning-ignore:unused_variable
 	var collision_info = move_and_collide(velocity * delta)
 
